eef6dfd05178adc1cd4cade26b4d185c044995c6
ivy <63594364+iv-y@users.noreply.github.com>
2021-11-21 14:51:09 +0900

    Add train and test data loading pipeline

--- simple_classifier.py
+++ simple_classifier.py
@@ -1,12 +1,13 @@
 import time
+from typing import Callable
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 import torch.optim as optim
+import torchvision
 from torchvision import datasets, transforms
-# from torchsummary.torchsummary import summary
 
 from models.simple_classify import Classifier3d
 
@@ -71,16 +72,58 @@ def test(net : nn.Module, device, test_loader):
     print('] Test loss: {:6.3f}, Test acc: {:7.3f}%'.format(test_loss, test_acc), end='')
 
 
+def filename_2_video(name: str) -> torch.Tensor:
+    video = torchvision.io.read_video(name)[0]
+    return torch.permute(video, (3, 0, 1, 2))
+
+def discriminate(mode: str) -> Callable[[str], bool]:
+
+    assert mode in ['train', 'test']
+    train = mode == 'train'
+
+    def decide(name: str) -> bool:
+        if 'RSP' in name:
+            return False
+
+        num_of_clip = int(name.split()[2].split('o')[0])
+        return train ^ (num_of_clip % 10 == 0)
+
+    return decide
+
+
 def main():
+
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224))
+    ])
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224))
+    ])
+
+    train_dataset = datasets.DatasetFolder(
+        'dataset', 
+        filename_2_video, 
+        transform=train_transform, 
+        is_valid_file=discriminate('train')
+    )
+    test_dataset = datasets.DatasetFolder(
+        'dataset', 
+        filename_2_video, 
+        transform=test_transform, 
+        is_valid_file=discriminate('test')
+    )
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True)
+    print(len(train_loader), len(test_loader))
 
     # Original ResNext101 size [64, 128, 256, 512, 1024], [3, 24, 36, 3]
     # Just can't fit that model into my GPU
 
-    net = Classifier3d(
-        channels_list=[64, 128, 128, 128, 128],
-        block_nums=[3, 4, 6, 3]
-    )
-
+    # net = Classifier3d(
+    #     channels_list=[64, 128, 128, 128, 128],
+    #     block_nums=[3, 4, 6, 3],
+    # )
 
 if __name__ == '__main__':
     main()
