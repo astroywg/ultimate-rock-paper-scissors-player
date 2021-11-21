d8546817c915f86ff21e2b0dff3fe41b89a4a14b
ivy <63594364+iv-y@users.noreply.github.com>
2021-11-21 22:08:49 +0900

    Combine data loading and train/test logic

--- simple_classifier.py
+++ simple_classifier.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 from typing import Callable
 
 import torch
@@ -9,6 +10,8 @@ import torch.optim as optim
 import torchvision
 from torchvision import datasets, transforms
 
+from tqdm import tqdm
+
 from models.simple_classify import Classifier3d
 
 
@@ -17,7 +20,7 @@ MIN_CLIP_FRAME_NUM = 87
 
 def train(net : nn.Module, device, train_loader, optimizer, epoch: int):
 
-    print('Epoch {:#2d} ['.format(epoch), end='')
+    print('Epoch {:#2d}'.format(epoch))
     net.train()
 
     criterion = nn.CrossEntropyLoss()
@@ -25,27 +28,30 @@ def train(net : nn.Module, device, train_loader, optimizer, epoch: int):
     loss_list = []
     acc_list = []
     
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data = data.to(device)
-        target = target.to(device)
+    with tqdm(total=len(train_loader), ncols=100, desc='Train') as t:
+        for data, target in train_loader:
+            data = data.to(device)
+            target = target.to(device)
 
-        output = net(data)
-        loss = criterion(output, target)
-        acc = (output.argmax(1) == target).float().mean()
+            output = net(data)
+            loss = criterion(output, target)
+            acc = (output.argmax(1) == target).float().mean()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        loss_list.append(loss.item())
-        acc_list.append(acc.item())
-        if batch_idx % (len(train_loader) // 20) == 0:
-            print('=', end='')
+            loss_list.append(loss.item())
+            acc_list.append(acc.item())
+            
+            t.set_postfix(loss=loss.item())
+            t.update()
 
     train_loss = sum(loss_list) / len(loss_list)
     train_acc = 100. * sum(acc_list) / len(acc_list)
         
-    print('] Train loss: {:6.3f}, Train acc: {:7.3f}%'.format(train_loss, train_acc), end='')
+    print('Train loss: {:6.3f}, Train acc: {:7.3f}%'.format(train_loss, train_acc))
+    return train_loss, train_acc
 
 
 def test(net : nn.Module, device, test_loader):
@@ -57,30 +63,44 @@ def test(net : nn.Module, device, test_loader):
     loss_list = []
     acc_list = []
 
-    with torch.no_grad():
-        for data, target in test_loader:
-            data = data.to(device)
-            target = target.to(device)
+    with tqdm(total=len(test_loader), ncols=100, desc='Train') as t:
+        with torch.no_grad():
+            for data, target in test_loader:
+                data = data.to(device)
+                target = target.to(device)
 
-            output = net(data)
-            loss = criterion(output, target)
-            acc = (output.argmax(1) == target).float().mean()
-    
-            loss_list.append(loss.item())
-            acc_list.append(acc.item())
+                output = net(data)
+                loss = criterion(output, target)
+                acc = (output.argmax(1) == target).float().mean()
+        
+                loss_list.append(loss.item())
+                acc_list.append(acc.item())
+
+                t.set_postfix(loss=loss.item())
+                t.update()
 
     test_loss = sum(loss_list) / len(loss_list)
     test_acc = 100. * sum(acc_list) / len(acc_list)
         
-    print('] Test loss: {:6.3f}, Test acc: {:7.3f}%'.format(test_loss, test_acc), end='')
+    print('Test loss: {:6.3f}, Test acc: {:7.3f}%'.format(test_loss, test_acc), end='')
+    return test_loss, test_acc
 
 
-def filename_2_video(name: str) -> torch.Tensor:
+def load_from_filename(name: str) -> torch.Tensor:
     video = torchvision.io.read_video(name)[0]
     frames = video.shape[0]
     offset = (frames - MIN_CLIP_FRAME_NUM) // 2
-    video = video[offset:(offset+MIN_CLIP_FRAME_NUM)]
-    return torch.permute(video, (3, 0, 1, 2))
+    video = video[offset:(offset+MIN_CLIP_FRAME_NUM)] 
+
+    images = list(video.numpy())
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224)),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    stacked = torch.stack([transform(image) for image in images]) # shape: (T, C, H, W)
+    return torch.transpose(stacked, 0, 1) # shape: (C, T, H, W)
+    
 
 def discriminate(train: bool) -> Callable[[str], bool]:
 
@@ -94,31 +114,34 @@ def discriminate(train: bool) -> Callable[[str], bool]:
     return decide
 
 
-def main():
+def save_model(net, path=None):
+    if path is None:
+        path = 'ckpts/simple_classify/' + datetime.now().strftime('%Y%m%d_%H%M%S') + '.pt'
+    torch.save(net.state_dict(), path)
+    print(' -> Saved', end='')
 
-    train_transform = transforms.Compose([
-        transforms.Resize((224, 224))
-    ])
-    test_transform = transforms.Compose([
-        transforms.Resize((224, 224))
-    ])
+
+def main():
 
     train_dataset = datasets.DatasetFolder(
         'dataset', 
-        filename_2_video, 
-        transform=train_transform, 
+        load_from_filename, 
         is_valid_file=discriminate(train=True)
     )
     test_dataset = datasets.DatasetFolder(
         'dataset', 
-        filename_2_video, 
-        transform=test_transform, 
+        load_from_filename, 
         is_valid_file=discriminate(train=False)
     )
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=2, shuffle=True)
-    print(len(train_loader), len(test_loader))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True)
+
+    print('Loaded', len(train_loader), 'train data,', len(test_loader), 'test data')
+
+    # settings
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Original ResNext101 size [64, 128, 256, 512, 1024], [3, 24, 36, 3]
     # Just can't fit that model into my GPU
@@ -126,7 +149,18 @@ def main():
     net = Classifier3d(
         channel_nums=[64, 128, 128, 128, 128],
         block_nums=[3, 4, 6, 3],
-    )
+    ).to(device)
+    optimizer = optim.Adam(net.parameters(), lr=1e-2, weight_decay=1e-4)
+
+    # train/test pipeline
+    best_acc = 0
+
+    for epoch in range(50):
+        train(net, device, train_loader, optimizer, epoch)
+        loss, acc = test(net, device, test_loader)
+
+        if acc > best_acc: save_model(net)
+        print()
 
 if __name__ == '__main__':
     main()
