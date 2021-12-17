abb385d42a8f45ae1ff0e2d4e0596819b4fbe62a
ivy <63594364+iv-y@users.noreply.github.com>
2021-12-17 17:15:52 +0900

    Optimize webcam input with multi-threading

--- webcam_input.py
+++ webcam_input.py
@@ -1,68 +1,179 @@
 import cv2
 import torch
 import numpy as np
-from queue import Queue
 from models.simple_classify import Classifier3d, Classifier2dLSTM
+
+import threading, time
 
 torch.backends.cudnn.benchmark = True
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with torch.no_grad():
 
-    # net = Classifier3d(
-    #         channel_nums=[16, 16, 32, 64, 128],
-    #         block_nums=[1, 1, 1, 1],
-    #     ).to(device)
+class SSRContext:
+    '''
+    Start-Stop-Release Context abstract class.
+    Calls start at enter, stop and release at exit.
+    '''
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
+        self.release()
+        return False
+
+
+class WebcamFrame(SSRContext):
+    '''
+    Webcam Frame updating with multi-threading.
+    Referred to https://github.com/PyImageSearch/imutils/blob/master/imutils/video/webcamvideostream.py
+    '''
+    def __init__(self, src=0):
+        self.capture = cv2.VideoCapture(src, cv2.CAP_DSHOW)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        self.lock = threading.Lock()
+        self.frame = None
+        self.stopped = False
+        self.release = self.capture.release
+
+    def start(self):
+        self.stopped = False
+        ret, frame = self.capture.read()
+        if ret:
+            self.frame = frame
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        while True:
+            if self.stopped:
+                return
+            ret, frame = self.capture.read()
+            if ret:
+                with self.lock:
+                    self.frame = frame
+
+    def get(self) -> np.ndarray:
+        with self.lock:
+            return self.frame
+
+    def stop(self):
+        self.stopped = True
+        self.thread.join()
+
+
+class FrameBuffer:
+    def __init__(self, size=1):
+        self.size = size
+        self.frames = np.zeros((size, 3, 240, 320))
+        self.pos = 0
+        self.lock = threading.Lock()
+
+    def put(self, frame):
+        processed = np.ascontiguousarray(frame.transpose(2, 0, 1)[np.newaxis])
+        with self.lock:
+            self.frames[self.pos % self.size] = processed
+            self.pos += 1
+
+    def to_array(self):
+        with self.lock:
+            if self.pos < self.size:
+                return self.frames[:self.pos]
+            else:
+                idx = self.pos % self.size
+                return np.concatenate((self.frames[idx:], self.frames[:idx]))
+
+
+class BufferedWebcamFrames(SSRContext):
+    '''
+    Thread-safe buffered webcam frames. 
+    Using WebcamFrame, Framebuffer, and an additional thread, a buffer is kept being updated.
+    '''
+
+    def __init__(self, fps, src=0, size=1):
+        self.webcam_frame = WebcamFrame(src)
+        self.buffer = FrameBuffer(size)
+        self.stopped = False
+        self.start_time = None
+        self.interval = 1/fps
+        self.release = self.webcam_frame.release
+
+    def start(self):
+        self.stopped = False
+        self.webcam_frame.start()
+        self.buffer.put(self.webcam_frame.get())
+        self.start_time = time.time()
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        while True:
+            if self.stopped:
+                return
+            start = time.time()
+            self.buffer.put(self.webcam_frame.get())
+            if time.time() - start > self.interval / 1.5:
+                print("[WARNING] Frames are processed slowly")
+            time.sleep(self.interval - ((time.time() - self.start_time) % self.interval))
+
+    def get(self) -> np.ndarray:
+        '''
+        Returns numpy array of shape (T, C, H, W).
+        '''
+        return self.buffer.to_array()
+
+    def now(self) -> np.ndarray:
+        '''
+        Returns the most recently captured image as numpy array.
+        The shape is (H, W, C).
+        '''
+        return self.webcam_frame.get().copy()
+
+    def stop(self):
+        self.stopped = True
+        self.thread.join()
+
+
+@torch.no_grad()
+def main_2dlstm():
+    '''
+    Main routine for 2D CNN + LSTM network.
+    Referred to:
+
+    - cv2 window management: https://stackoverflow.com/a/45564409
+    '''
+
     net = Classifier2dLSTM().to(device)
 
     net.load_state_dict(torch.load('Classifier2dLSTM.pt', map_location=device))
     net.eval()
 
-    video_b, video_g, video_r = Queue(128), Queue(128), Queue(128)
-    # table = ["misc", "rock", "scissors", "paper"]
     win_table = ["misc", "paper", "rock", "scissors"]
 
-    capture = cv2.VideoCapture(0)
+    with BufferedWebcamFrames(fps=30, size=1) as f:
+        state = None
+        while True:
+            frame = f.get().squeeze()
+            features = net.get_features(frame, device)
+            output, state = net.next_output(features, state)
+            idx = int(output[-1].argmax())
+            result = win_table[idx]
 
-    def read_and_put():
-        ret, color_img = capture.read()
-        if not ret: raise()
+            screen = f.now()
+            cv2.putText(screen, result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            cv2.imshow('Result', screen)
+            cv2.waitKey(1)
 
-        b, g, r = cv2.split(color_img)
-        b = cv2.resize(b, (320, 240))
-        g = cv2.resize(g, (320, 240))
-        r = cv2.resize(r, (320, 240))
+            if cv2.getWindowProperty('Result', cv2.WND_PROP_VISIBLE) < 1:
+                break
 
-        video_b.put(b)
-        video_g.put(g)
-        video_r.put(r)
-
-        return b, g, r
-
-    for i in range(128):
-        b, g, r = read_and_put()
-    
-    while capture.isOpened():
-        video_b.get()
-        video_g.get()
-        video_r.get()
-
-        b, g, r = read_and_put()
-
-        video = np.array([np.array((list(video_b.queue), list(video_g.queue), list(video_r.queue)))])
-        video_capture = torch.from_numpy(video).to(device).float().div(255)
-        video_capture.sub_(0.5).div_(0.5)
-
-        result = net(video_capture)[0]
-        result = result[-1].argmax()
-        result = win_table[int(result)]
-
-        frame = np.dstack([b, g, r])
-        cv2.putText(frame, result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-        cv2.imshow("Result", frame)
-        cv2.waitKey(0)
-
-        print(result)
-
-    capture.release()
     cv2.destroyAllWindows()
+
+
+def main():
+    main_2dlstm()
+
+if __name__ == '__main__':
+    main()
